0e880178e54e32915b1970544a3f3e6a22d618c1
Thies Mueller <github@td00.de>
2022-11-04 19:48:12 +0100

    updated js

---
+++ js/wasisteinehackerin.js
@@ -0,0 +1,39 @@
+
+var headerText = "Was ist eigentlich eine Hacker*in?";
+
+var footerText = "<a href='https://github.com/td00/alpakabingo' target='_blank'>Code available on github</a>";
+
+var winText = "Gewonnen";
+
+var clickSnd = new Audio("audio/click.mp3");
+
+var winSnd = new Audio("audio/win.mp3");
+
+var JSONBingo = {"squares": [
+        {"square": "Grüne Schrift"},
+        {"square": "Papier"},
+        {"square": "Facebook"},
+        {"square": "Software"},
+        {"square": "Künstliche Intelligenz"},
+        {"square": "Bug"},
+        {"square": "Hardware"},
+        {"square": "NASA"},
+        {"square": "3D-Drucker"},
+        {"square": "Militär"},
+        {"square": "Lochkarten"},
+        {"square": "Open-Source"},
+        {"square": "Hacker*in"},
+        {"square": "Anonymous"},
+        {"square": "Polizei"},
+        {"square": "Mastodon"},
+        {"square": "Maskulin"},
+        {"square": "Telefon"},
+        {"square": "Dezentral"},
+        {"square": "Virtual Reality"},
+        {"square": "Geheimdienst"},
+        {"square": "Twitter"},
+        {"square": "Gesetz"},
+        {"square": "Terminal"},
+        {"square": "Sonnenbrille"},
+    ]
+};
