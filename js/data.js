92f075118b2f2983849a5d543429d4c869adfcb1
Thies Mueller <github@td00.de>
2022-11-04 19:39:53 +0100

    initial files

---
+++ js/data.js
@@ -0,0 +1,18 @@
+
+var headerText = "Was ist eigentlich eine Hacker*in?";
+
+var footerText = "<a href='https://github.com/td00/alpakabingo' target='_blank'>Code available on github</a>";
+
+var winText = "Gewonnen";
+
+var clickSnd = new Audio("audio/click.mp3");
+
+var winSnd = new Audio("audio/win.mp3");
+
+var JSONBingo = {"squares": [
+        {"square": "Grüne Schrift"},
+        {"square": "Papier"},
+        {"square": "Facebook"},
+    {"square": "Welche Metsorten habt ihr denn?"},
+    ]
+};
