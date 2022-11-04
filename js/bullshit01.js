683049afa7c30d16bff6ffa8ac42e9fb6338af97
Thies Mueller <github@td00.de>
2022-11-05 00:08:29 +0100

    added bingos

---
+++ js/bullshit01.js
@@ -0,0 +1,38 @@
+
+var headerText = "Bullshit Bingo";
+
+var footerText = "<a href='https://github.com/td00/alpakabingo' target='_blank'>Code available on github</a>";
+
+var winText = "Gewonnen";
+
+var clickSnd = new Audio("audio/click.mp3");
+
+var winSnd = new Audio("audio/win.mp3");
+
+var JSONBingo = {"squares": [
+        {"square": "Blockchain"},
+        {"square": "NFT"},
+        {"square": "KI"},
+        {"square": "Agil"},
+        {"square": "Lifecycle"},
+        {"square": "Learning"},
+        {"square": "Cloud"},
+        {"square": "Serverless"},
+        {"square": "Cyber"},
+        {"square": "Lessons Learned"},
+        {"square": "Web3.0"},
+        {"square": "Cryptocurrency"},
+        {"square": "Secure"},
+        {"square": "Alogrithmus"},
+        {"square": "Game Changer"},
+        {"square": "16"},
+        {"square": "17"},
+        {"square": "18"},
+        {"square": "19"},
+        {"square": "20"},
+        {"square": "21"},
+        {"square": "22"},
+        {"square": "23"},
+        {"square": "24"},
+    ]
+};
---
+++ js/zitate.js
@@ -0,0 +1,38 @@
+
+var headerText = "Zitate Bingo";
+
+var footerText = "<a href='https://github.com/td00/alpakabingo' target='_blank'>Code available on github</a>";
+
+var winText = "Gewonnen";
+
+var clickSnd = new Audio("audio/click.mp3");
+
+var winSnd = new Audio("audio/win.mp3");
+
+var JSONBingo = {"squares": [
+        {"square": "1"},
+        {"square": "2"},
+        {"square": "3"},
+        {"square": "4"},
+        {"square": "5"},
+        {"square": "6"},
+        {"square": "7"},
+        {"square": "8"},
+        {"square": "9"},
+        {"square": "10"},
+        {"square": "11"},
+        {"square": "12"},
+        {"square": "13"},
+        {"square": "14"},
+        {"square": "15"},
+        {"square": "16"},
+        {"square": "17"},
+        {"square": "18"},
+        {"square": "19"},
+        {"square": "20"},
+        {"square": "21"},
+        {"square": "22"},
+        {"square": "23"},
+        {"square": "24"},
+    ]
+};
